2b3dcdfc7707710df18623738a744cd8bf83c8be
plass <andreas.plass@haw-hamburg.de>
2013-11-15 09:35:22 +0100

    Aufgaben w8
    w8

--- Aufgaben/w8/StereoPlugin/source/plugin.cpp
+++ Aufgaben/w8/StereoPlugin/source/plugin.cpp
@@ -1,15 +1,20 @@
 #include "plugin.h"
-
+#include <math.h>
 #include <stdio.h>
 
 namespace Steinberg {
 namespace Vst {
 
+
+const float GAIN_DEFAULT_DB = 0;
+const float GAIN_MIN_DB = -100;
+const float GAIN_MAX_DB = 6;
 //-----------------------------------------------------------------------------
 // member function of PluginController!
 // define parameter definitions here...
 void PluginController::setupParameters(){
-	parameters.addParameter(new RangeParameter(STR16("Gain"), kGainId, STR16("%"), 0, 100, 100));
+	parameters.addParameter(new RangeParameter(STR16("Gain"), kGainId, STR16("dB"), GAIN_MIN_DB, GAIN_MAX_DB, GAIN_DEFAULT_DB));
+	parameters.addParameter(new RangeParameter(STR16("Balance"), kBalanceId, STR16("%"), -100, 100, 0));
 
 	// fix for RangeParameter (default value is not yet set)
 	for(int i = 0; i < parameters.getParameterCount(); i++){
@@ -23,6 +28,8 @@ void PluginController::setupParameters(){
 Plugin::Plugin ()
 : numChannels(0)
 , sampleRate(0)
+, gain(1)
+, balance(0.5f)
 {
 	setControllerClass (PluginControllerUID);
 }
@@ -35,11 +42,29 @@ void Plugin::startProcessing(int numChannels, SampleRate sampleRate){
 }
 tresult PLUGIN_API Plugin::process (ProcessData& data)
 {
-    if (hasInputParameterChanged(data, kGainId)){
-        float gain = getInputParameterChange(data, kGainId);
-		leftProcessor.setGain(gain);
-		rightProcessor.setGain(gain);
+	if (hasInputParameterChanged(data, kGainId)){
+        gain = getInputParameterChange(data, kGainId);
     }
+    if (hasInputParameterChanged(data, kBalanceId)){
+        balance = getInputParameterChange(data, kBalanceId);
+    }
+    
+	// Umrechnung Parameter 0...1 in dB: -100 ... 0 dB
+	float db = gain * 100 - 100;
+	float factor = pow(10, db/20);
+
+	float gainLeft = factor;
+	float gainRight = factor;
+	if (balance < 0.5){
+		gainRight *= (2 * balance);
+	}
+	else{
+		gainLeft *= (2* (1 - balance));
+	}    
+	leftProcessor.setGain(gainLeft);
+	rightProcessor.setGain(gainRight);
+
+
  	if (numChannels > 0){
 		float* leftInputChannel = data.inputs[0].channelBuffers32[0];
 		float* leftOutputChannel = data.outputs[0].channelBuffers32[0];
